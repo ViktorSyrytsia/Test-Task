c6a1fc3008d54d89588ff5c0a77f1238df5d3222
Oleh-Viktor <syrytsiaviktor@gmail.com>
2020-10-23 23:02:58 +0300

    task 1 done

--- 1_Change-form/script.js
+++ 1_Change-form/script.js
@@ -1,20 +1,35 @@
 document.forms.change_form.onsubmit = (e) => {
   e.preventDefault();
-  const change =
-    document.getElementById('sum').value -
-    document.getElementById('price').value;
-  const rest = change.toFixed(2);
-  const cents = rest.split('.')[1];
-  const nom = nominals(cents);
-  console.log(nom);
+  const sum = document.getElementById('sum').value;
+  const price = document.getElementById('price').value;
+  if (price > sum) {
+    alert("You don't have enough money");
+  }
+  const change = (sum - price).toFixed(2);
+  const cents = change.split('.')[1];
+  const centsNominal = nominals(cents);
+  console.log(centsNominal);
+  const dollars = change.split('.')[0];
 
-  alert(
-    `${rest.split('.')[0] <= 0 ? '' : rest.split('.')[0] + ' dollars,'} ${
-      rest.split('.')[1] < 10
-        ? rest.split('.')[1][1] + ' cents'
-        : rest.split('.')[1] + ' cents'
-    }`
-  );
+  const changeOutput = document.getElementById('change');
+  changeOutput.innerHTML = 'Your change is: ';
+  changeOutput.innerHTML += `${
+    change.split('.')[0] <= 0 ? '' : change.split('.')[0] + ' dollars,'
+  } ${
+    change.split('.')[1] < 10
+      ? change.split('.')[1][1] + ' cents'
+      : change.split('.')[1] + ' cents'
+  }`;
+
+  nominalsOutput = document.getElementById('nominals');
+  nominalsOutput.innerHTML = 'By nominal value: ';
+  nominalsOutput.innerHTML += `${dollars <= 0 ? '' : dollars + ' dollars'}
+    ${centsNominal[50] > 0 ? centsNominal[50] + 'x50cent' : ''} 
+    ${centsNominal[25] > 0 ? centsNominal[25] + 'x25cent' : ''} 
+    ${centsNominal[10] > 0 ? centsNominal[10] + 'x10cent' : ''} 
+    ${centsNominal[5] > 0 ? centsNominal[5] + 'x5cent' : ''} 
+    ${centsNominal[1] > 0 ? centsNominal[1] + 'x1cent' : ''} `;
+
   document.getElementById('sum').value = 0;
   document.getElementById('price').value = 0;
 };
@@ -31,13 +46,12 @@ function nominals(cents) {
     value -= 25;
   }
   if (value >= 10) {
-    const trace = (value / 10).toFixed(0);
-    let couter = trace;
-    while (couter > 0) {
+    let trace = value / 10;
+    while (trace >= 1) {
       nom[10]++;
-      couter--;
+      trace--;
     }
-    value = value - trace * 10;
+    value = value - nom[10] * 10;
   }
   if (value >= 5) {
     nom[5]++;
