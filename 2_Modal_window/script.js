a8f835fa455215ea5d4c48eb35768f3e08bb6860
Oleh-Viktor <syrytsiaviktor@gmail.com>
2020-10-24 12:31:11 +0300

    add smooth animation to modal window

--- 2_Modal_window/script.js
+++ 2_Modal_window/script.js
@@ -1,10 +1,11 @@
-document.getElementById('create_modal').onclick = (e) => {
-  e.preventDefault();
-
+document.getElementById('create_modal').onclick = () => {
   const modal = document.createElement('div');
-  // modal.className = 'modal';
-  modal.tabIndex = -1;
-
+  modal.className = 'modal';
+  modal.style.display = 'block';
+  modal.style.transition = 'all 0.3s';
+  setTimeout(() => {
+    modal.style.backgroundColor = 'rgba(25,25,25,0.8)';
+  }, 10);
   const modalDialog = document.createElement('div');
   modalDialog.className = 'modal-dialog';
   modal.appendChild(modalDialog);
@@ -19,6 +20,11 @@ document.getElementById('create_modal').onclick = (e) => {
   closeButton.type = 'button';
   closeButton.className = 'close';
   closeButton.innerHTML = 'x';
+  closeButton.onclick = () => {
+    modal.style.backgroundColor = 'rgba(25,25,25,0.0)';
+    modal.style.opacity = '0';
+    setTimeout(() => modal.remove(), 300);
+  };
   modalHeader.className = 'modal-header';
   header.className = 'modal-title';
   header.innerHTML = 'Modal title';
@@ -40,9 +46,17 @@ document.getElementById('create_modal').onclick = (e) => {
   okButton.className = 'btn btn-success';
   okButton.innerHTML = 'OK';
   okButton.type = 'button';
+  okButton.onclick = () => {
+    alert('Hello from modal');
+  };
   noButton.className = 'btn btn-danger';
   noButton.innerHTML = 'NO';
   noButton.type = 'button';
+  noButton.onclick = () => {
+    modal.style.backgroundColor = 'rgba(25,25,25,0.0)';
+    modal.style.opacity = '0';
+    setTimeout(() => modal.remove(), 300);
+  };
   modalFooter.appendChild(okButton);
   modalFooter.appendChild(noButton);
   modalContent.appendChild(modalFooter);
